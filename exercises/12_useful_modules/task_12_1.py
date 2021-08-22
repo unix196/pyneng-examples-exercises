a1a7f18b1937c07b430ffb5a6cb92d92eb2b38bc
unix196 <unix196@gmail.com>
2021-08-22 22:25:27 +0500

    22.08.2021

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+import sys
+
+ips = sys.argv[1][1:-1].split(",") #убираем кв. скобки из переданного аргумента, разделяем строку по запятой
+def ping_ip_addresses(list_ips):
+    #print(list_ips)
+    ips_tuple = ()
+    valid_ip = []
+    bad_ip = []
+    for ip in list_ips:
+        result = subprocess.run(["ping", "-c", "1", ip], stdout=subprocess.DEVNULL)
+        if result.returncode == 0:
+            valid_ip.append(ip)
+        else:
+            bad_ip.append(ip)
+    ips_tuple = (valid_ip, bad_ip)
+    return ips_tuple
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(ips))
