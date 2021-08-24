1606b910825366204c66f0b2e438b5c61d25c022
unix196 <unix196@gmail.com>
2021-08-24 09:51:59 +0500

    24.08.2021

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -34,3 +34,32 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+import ipaddress
+
+def convert_ranges_to_ip_list(ip_list):
+    summary_list = []
+    for ips in ip_list:
+        if "-" in ips:
+            start_ip, stop_ip = ips.split("-")
+            start=ipaddress.ip_address(start_ip)
+            try:
+                stop=ipaddress.ip_address(stop_ip)
+                #print(stop)
+            except:
+                #убираем последний октет и добавляем сразу; start_ip.split(".")[:-1] возвращает ['1', '1', '1']
+                end_ip= '{}.{}'.format(".".join(start_ip.split(".")[:-1]),stop_ip)
+                stop=ipaddress.ip_address(end_ip)
+                #print(stop)
+                #print(end_ip)
+            #print(start_ip, stop)
+            for ip_int in range(int(start), int(stop)+1):
+                #print(ipaddress.IPv4Address(ip_int))
+                summary_list.append(str(ipaddress.ip_address(ip_int)))
+        else:
+            summary_list.append(ips)
+
+    return summary_list
+
+
+if __name__ == "__main__":
+    print(convert_ranges_to_ip_list(['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']))
