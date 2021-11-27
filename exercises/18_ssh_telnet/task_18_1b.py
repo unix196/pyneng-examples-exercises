ed0c166cc160bdca2d97132cdac4423d3ea387c4
unix196 <unix196@gmail.com>
2021-11-27 22:13:47 +0500

    27.11.2021

--- exercises/18_ssh_telnet/task_18_1b.py
+++ exercises/18_ssh_telnet/task_18_1b.py
@@ -12,3 +12,23 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 """
+
+import yaml
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetmikoTimeoutException
+from netmiko.ssh_exception import AuthenticationException
+
+def send_show_command(dev, command):
+    try:
+        ssh = ConnectHandler(**dev)
+        return ssh.send_command(command)
+    except (AuthenticationException, NetmikoTimeoutException) as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    command = "sh ip int br"
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    for dev in devices:
+        print(send_show_command(dev, command))
